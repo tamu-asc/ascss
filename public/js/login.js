2274b75651265699e8f961035c0898afcc4ebd04
Utkarsh Chanchlani <utkarsh.chanchlani@gmail.com>
2019-03-27 00:31:01 -0500

    login signup complete

---
+++ public/js/login.js
@@ -0,0 +1,21 @@
+
+
+$.ajaxSetup({
+    contentType: "application/json; charset=utf-8"
+});
+
+$(document).ready(function () {
+
+    $("#loginButton").click(function () {
+        var loginCreds = {
+            "user": {
+                "email": $("#inputEmail").val(),
+                "password": $("#inputPassword").val()
+            }
+        };
+
+        $.post("/api/login", JSON.stringify(loginCreds), function (data) {
+            alert("Data: " + JSON.stringify(data));
+        });
+    });
+});
---
+++ public/js/register.js
@@ -0,0 +1,25 @@
+
+
+$.ajaxSetup({
+    contentType: "application/json; charset=utf-8"
+});
+
+$(document).ready(function () {
+
+    $("#registerButton").click(function () {
+        var loginCreds = {
+            "user": {
+                "first_name": $("#firstName").val(),
+                "last_name": $("#lastName").val(),
+                "username": $("#userName").val(),
+                "email": $("#inputEmail").val(),
+                "password": $("#inputPassword").val(),
+                "password_confirmation": $("#inputPasswordConfirmation").val()
+            }
+        };
+
+        $.post("/api/signup", JSON.stringify(loginCreds), function (data) {
+            alert("Data: " + JSON.stringify(data));
+        });
+    });
+});
