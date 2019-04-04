e91a9047f957b4fd1c5d16f44a9829c8cf91549a
Utkarsh Chanchlani <utkarsh.chanchlani@gmail.com>
2019-04-04 00:45:46 -0500

    issue migration

--- db/migrate/20190326231942_create_session_attendances.rb
+++ db/migrate/20190326231942_create_session_attendances.rb
@@ -2,7 +2,7 @@ class CreateSessionAttendances < ActiveRecord::Migration[5.2]
   def change
     create_table :session_attendances do |t|
       t.references :session, foreign_key: true
-      t.references :student_course, foreign_key: true
+      t.references :course_student, foreign_key: true
       t.datetime :in_time
 
       t.timestamps
