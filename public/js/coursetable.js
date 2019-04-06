b767bf5c15bf814c89314304b4e007c462c63bd9
jatin kamnani <jkam273@tamu.edu>
2019-04-06 17:42:11 -0500

    api integration + route block

--- public/js/coursetable.js
+++ public/js/coursetable.js
@@ -1,26 +1,34 @@
-//$.getJSON('js/courses.json',
-//function(data){
-    //console.log(data);
-    //var items=[]
-    //$(data).each(function(i,course){
-    //items.push('<a href="#" class="list-group-item list-group-item-action list-group-item-dark"></a>');        
-    //items.push('<li class="list-group-item list-group-item-secondary">'+course.code+'   -   '+course.title+'</li>');
-    //        
-    //});
-    //$('#dynamiclistbody').append(items.join(''));
-    //})
+$(document).ready(function() {
+    if(localStorage.getItem("login") != "TRUE") {
+        window.location.assign("/index")
+    }
+    else {
+    $.getJSON('api/student/courses',
+    function(data,message){
+        $(data.courses).each(function(i,course){
+            console.log(course)
+            $('#CoursesBody').append($("<tr>")
+                .append($("<th>").append(course.id))
+                .append($("<td>").append("CSE606"))
+                .append($("<td>").append(course.title))
+                .append($("<td>").append(course.semester))
+                .append($("<td>").append(course.year))
+                .append($("<td>").append(course.credits))
+                .append($('<td><a class="btn btn-primary btn-sm" href="#" role="button">View</a>')));
+        });
+    },function(err) {
+        alert(err);
+    })         
 
-$.getJSON('js/courses.json',
-function(data){
-    //console.log(data);
-    $(data).each(function(i,course){
-        $('#CoursesBody').append($("<tr>")
-            .append($("<th>").append(course.id))
-            .append($("<td>").append(course.code))
-            .append($("<td>").append(course.title))
-            .append($("<td>").append(course.semester))
-            .append($("<td>").append(course.year))
-            .append($("<td>").append(course.credits))
-            .append($('<td><a class="btn btn-primary btn-sm" href="#" role="button">View</a>')));
+
+    $("#logout").click(function () {
+        $.post("/api/logout", function (data) {
+            localStorage.setItem("login", "FALSE");
+            window.location.assign('/index')
+        });
     });
-})         
+    }   
+    
+})
+
+
