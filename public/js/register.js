a580fd26fead3de119ff5784a85bdfb65647496c
Ranjith Tamil Selvan <ranjithtamizhchelvan@gmail.com>
2019-05-01 12:22:51 -0500

    Register page validations and error handling

--- public/js/register.js
+++ public/js/register.js
@@ -23,13 +23,68 @@ $(document).ready(function () {
         };
         window.loginCreds = loginCreds;
         debugger;
-        if (loginCreds["user"]["first_name"] == "" || loginCreds["user"]["last_name"] == ""
+        if (loginCreds["user"]["first_name"] == "" || loginCreds["user"]["last_name"] == "" || loginCreds["user"]["last_name"] == undefined
         || loginCreds["user"]["first_name"] == undefined || loginCreds["user"]["username"] == ""
         || loginCreds["user"]["username"] == undefined ||
         !expression.test(String(loginCreds["user"]["email"]).toLowerCase())
         ||  !(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/).test(loginCreds["user"]["password"])
         || loginCreds["user"]["password"]!==loginCreds["user"]["password_confirmation"]
       ) {
+
+        if (loginCreds["user"]["first_name"] == "" ||   loginCreds["user"]["first_name"] == undefined)
+        {
+          $('#firstnameflash').show();
+        }
+        else {
+          $('#firstnameflash').hide();
+        }
+
+        if(loginCreds["user"]["last_name"] == "" || loginCreds["user"]["last_name"] == undefined)
+        {
+ 	        $('#lastnameflash').show();
+        }
+        else {
+          $('#lastnameflash').hide();
+        }
+
+        if(loginCreds["user"]["username"] == ""  || loginCreds["user"]["username"] == undefined)
+        {
+ 	        $('#usernameflash').show();
+        }
+        else {
+          $('#usernameflash').hide();
+        }
+
+        if(!expression.test(String(loginCreds["user"]["email"]).toLowerCase()) )
+        {
+ 	        $('#emailflash').show();
+        }
+        else {
+          $('#emailflash').hide();
+        }
+
+
+        if( !(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/).test(loginCreds["user"]["password"]) )
+        {
+ 	        $('#passwordflash').show();
+        }
+        else {
+          $('#passwordflash').hide();
+        }
+
+
+        if( loginCreds["user"]["password"]!==loginCreds["user"]["password_confirmation"])
+        {
+ 	        $('#passwordconfirmflash').show();
+        }
+        else {
+          $('#passwordconfirmflash').hide();
+        }
+
+
+
+
+
 
         alert('please fill out all fields correctly')
         return
